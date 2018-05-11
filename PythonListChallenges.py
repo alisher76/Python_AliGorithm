7af7729f98e0b97d7637d91fc00d8aaca2c76852
AliGorithm <alisher.coder@gmail.com>
2018-05-11 14:20:24 -0500

    PythonListPrintNumsFromList

--- PythonListChallenges.py
+++ PythonListChallenges.py
@@ -47,5 +47,14 @@ def sumOfTwo(a=[],b=[],v=0):
             return True
     return False
 
-result = sumOfTwo([1,2,3,4],[10,20,30,40], 44)
-print(result)
+#result = sumOfTwo([1,2,3,4],[10,20,30,40], 44)
+#print(result)
+arr = 4
+print(arr.times)
+
+# Write a Python program to change the position of every n-th value with the (n+1)th in a list.
+def convertListOfNumsInOne(list_of_nums):
+    numString = x = int("".join(map(str, list_of_nums)))
+    return numString
+
+convertListOfNumsInOne([1,2,3,4,5])
