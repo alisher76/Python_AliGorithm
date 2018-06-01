e2107441fc0a2b5a8a536cab066193193d202dd0
AliGorithm <alisher.coder@gmail.com>
2018-06-01 15:38:38 -0500

    littleChange

---
+++ Data_type_based_tasks/String/py_files/unique_characters.py
@@ -0,0 +1,43 @@
+"""
+Problem
+Given a string,determine if it is compreised of all unique characters.
+For example, the string 'abcde' has all unique characters and should return True.
+The string 'aabcde' contains duplicate characters and should return false.
+
+Solution
+We'll show two possible solutions, one using a built-in data structure and a built in function,
+and another using a built-in data structure but using a look-up method to check if the characters are unique.
+"""
+
+def uni_char(s):
+    # simplest fastest way
+    return len(set(s)) == len(s)
+
+
+def uni_char2(s):
+    chars = set()
+    for letter in s:
+        if letter in chars:
+            return False
+        else:
+            chars.add(letter)
+    return True
+
+
+"""
+RUN THIS CELL TO TEST THE CODE>
+"""
+from nose.tools import assert_equal
+
+
+class TestUnique(object):
+
+    def test(self, sol):
+        assert_equal(sol(''), True)
+        assert_equal(sol('goo'), False)
+        assert_equal(sol('abcdefg'), True)
+        print('ALL TEST CASES PASSED')
+
+# Run Tests
+t = TestUnique()
+t.test(uni_char2)
